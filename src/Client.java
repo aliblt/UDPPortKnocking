4c029491960a040ff357420045d251d537bc062f
alibulut <alibulut96@gmail.com>
2020-01-12 21:22:45 +0100

    Initial Commit

---
+++ src/Client.java
@@ -0,0 +1,68 @@
+import java.io.*;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.util.ArrayList;
+
+public class Client {
+
+    public static void main(String[] args) throws IOException {
+        //First args is Server port number
+
+        if (args.length < 1) {
+            System.out.println("Insufficient # of args");
+            return;
+        }
+
+        InetAddress address = InetAddress.getLocalHost();
+        int serverPortNum = Integer.parseInt(args[0]);
+
+        DatagramSocket socket = new DatagramSocket();
+        int clientPortNum = socket.getLocalPort();
+
+
+        //Send 3 packets to server in order to activate
+        for (int i=0 ; i<1 ; i++ ) {
+
+            byte[] queryBuff = String.valueOf(i).getBytes();
+            DatagramPacket query = new DatagramPacket(queryBuff, queryBuff.length, address, serverPortNum);
+
+            socket.send(query);
+        }
+
+        System.out.println("Initial Messages has sent ");
+
+
+        byte[] buff = new byte[UDP.MAX_DATAGRAM_SIZE];
+        DatagramPacket packet = new DatagramPacket(buff, buff.length);
+
+        socket.receive(packet);
+
+        String str = new String(packet.getData(), 0, packet.getLength()).trim();
+        int serverDataPort = Integer.parseInt(str);
+
+        System.out.println("Received Port Number: "  + serverDataPort);
+
+        Socket dataSocket = new Socket("localhost", serverDataPort);
+        InputStream is = dataSocket.getInputStream();
+        BufferedReader incomingData = new BufferedReader(new InputStreamReader(dataSocket.getInputStream()));
+
+        String fileName = incomingData.readLine();
+        int dataSize = Integer.parseInt(incomingData.readLine());
+
+        System.out.println("Incoming data Name: " + fileName);
+        System.out.println("Data Size: " + dataSize);
+
+        FileOutputStream fos = new FileOutputStream("ClientData/"+fileName);
+        BufferedOutputStream bos = new BufferedOutputStream(fos);
+        bos.write(is.readNBytes(dataSize));
+        bos.flush();
+        fos.close();
+        bos.close();
+
+        dataSocket.close();
+        socket.close();
+    }
+
+}
---
+++ src/UDPServer.java
@@ -0,0 +1,96 @@
+import javax.xml.crypto.Data;
+import java.io.*;
+import java.net.*;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class UDPServer {
+    private List<DatagramSocket> serverPorts = new ArrayList<DatagramSocket>();
+
+    public UDPServer(String[] ports) throws SocketException  {
+        initializeServer(ports);
+    }
+
+    private void initializeServer(String[] ports) throws SocketException {
+        for (int i=0 ; i<ports.length ; i++ ) {
+            serverPorts.add(new DatagramSocket(Integer.parseInt(ports[i])));
+            System.out.println("Server started to listens on: " + serverPorts.get(i).getLocalPort());
+        }
+    }
+
+    private void service(DatagramSocket ds) {
+
+        while (true) {
+            try {
+                portListen(ds);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    private void portListen(DatagramSocket ds ) throws IOException{
+        byte[] buff = new byte[UDP.MAX_DATAGRAM_SIZE];
+        final DatagramPacket datagram = new DatagramPacket(buff, buff.length);
+
+        ds.receive(datagram);
+
+        new Thread(() ->  {
+            try {
+                int n = Integer.parseInt(new String(datagram.getData(), 0, datagram.getLength()));
+                System.out.println("I've got " + n);
+
+                ServerSocket dataSocket = new ServerSocket(0);
+
+                int dataPort = dataSocket.getLocalPort();
+
+                System.out.println("New Data Port: " + dataPort);
+
+                byte[] respBuff = String.valueOf(dataPort).getBytes();
+                int clientPort = datagram.getPort();
+                InetAddress clientAddress = datagram.getAddress();
+                DatagramPacket resp = new DatagramPacket(respBuff, respBuff.length, clientAddress, clientPort);
+                try {
+                    ds.send(resp);
+                    System.out.println("I've sent " + dataPort);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                File file = new File("ServerData/HelloWorld");
+                String fileName = file.getName();
+                int fileSize = (int) file.length();
+                byte[] dataFileByte = new byte[fileSize];
+                BufferedInputStream bis = new BufferedInputStream(new FileInputStream(file));
+
+                Socket s = dataSocket.accept();
+                PrintWriter out = new PrintWriter(s.getOutputStream(), true);
+                bis.read(dataFileByte, 0, fileSize);
+                System.out.println("Sending Data Name and Size: " + fileName + " " + fileSize);
+                out.println(fileName);
+                out.println(fileSize);
+                s.getOutputStream().write(dataFileByte,0,fileSize);
+                s.close();
+            }
+            catch (Exception e) {
+                e.printStackTrace();
+            }
+        }).start();
+    }
+
+    private void listen() {
+        for( DatagramSocket ds: serverPorts ) {
+            new Thread(() -> service(ds)).start();
+        }
+    }
+
+
+    public static void main(String[] args) {
+        try {
+            new UDPServer(args).listen();
+        } catch (SocketException e) {
+            System.out.println("Could not set up the server");
+        }
+    }
+
+}
